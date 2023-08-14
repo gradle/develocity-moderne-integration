f3c0edc54440f2e62cd36f287943688b09cfdb6a
Alexis Tual <atual@gradle.com>
2023-08-14 13:29:22 +0200

    Update README

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
 }
 
-rootProject.name = "gradle-build-scan-moderne"
+rootProject.name = "gradle-build-scan-moderne-integration"
